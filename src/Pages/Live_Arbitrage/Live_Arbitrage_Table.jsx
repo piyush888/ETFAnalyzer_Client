cebaa2dadc62f22088dc0afc07a1a1e6bdd0fdd1
piyush888 <piyush888@gmail.com>
2020-07-12 17:45:51 +0530

    Sorting done, sort icons done.

--- src/Pages/Live_Arbitrage/Live_Arbitrage_Table.jsx
+++ src/Pages/Live_Arbitrage/Live_Arbitrage_Table.jsx
@@ -14,9 +14,15 @@ const Live_Arbitrage_All_Table = (props) => {
   const [expandedRows, setExpandedRows] = useState([])
   const [orderType, setOrderType] = useState("ASC");
   const [searchString, setSearchString] = useState("");
+  // const [filterStringArb, setFilterStringArb] = useState();
+  // const [filterStringAbsArb, setFilterStringAbsArb] = useState();
+  // const [filterStringSpread, setFilterStringSpread] = useState();
   const [filterData, setFilterData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isSorted, setSorted] = useState(false);
+  const [isSortedBy, setSorted] = useState("");
+  // const inputArb = useRef(null);
+  // const inputAbsArb = useRef(null);
+  // const inputSpread = useRef(null);
 
 
   function useInterval(callback, delay) {
@@ -40,18 +46,18 @@ const Live_Arbitrage_All_Table = (props) => {
   }
 
   useEffect(() => {
-      if (tableData.length<1){
-            Axios.get(
-              `/ETfLiveArbitrage/AllTickers`
-            ).then(({ data }) => {
-              setTableData(data);
-              setFilterData(data);
-              setIsLoading(false)
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
+    if (tableData.length < 1) {
+      Axios.get(
+        `/ETfLiveArbitrage/AllTickers`
+      ).then(({ data }) => {
+        setTableData(data);
+        setFilterData(data);
+        setIsLoading(false)
+      })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   });
 
   useEffect(() => {
@@ -71,24 +77,57 @@ const Live_Arbitrage_All_Table = (props) => {
     setSearchString(e.target.value);
   };
 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     if (filterStringArb.length < 1 && tableData.length !== 0) {
+  //       return setFilterData(tableData);
+  //     }
+  //     if (tableData.length !== 0) {
+  //       let values = filterStringArb.split(',')
+  //       const col = values[0]
+  //       const binop = values[1]
+  //       const filval = values[2]
+
+  //     }
+  //   }, 300);
+  // }, [filterStringArb]);
+
+  // const handleFilterChange = (e) => {
+  //   setFilterStringArb(e.target.value);
+  // };
+
   const changeOrder = (e) => {
     if (orderType === "ASC") {
       const sortedData = orderBy(filterData, [e], ["asc"]);
       setOrderType("DSC");
       setFilterData(sortedData);
-      setSorted(true);
+      setSorted(e);
     }
     else if (orderType === "DSC") {
       const sortedData = orderBy(filterData, [e], ["desc"]);
       setOrderType("ASC");
       setFilterData(sortedData);
-      setSorted(true);
+      setSorted(e);
     }
   };
 
+  const getClassNameForSortIcon = (col) => {
+    if (isSortedBy === col) {
+      if (orderType === "ASC") {
+        return "fas fa-sort-down";
+      }
+      else if (orderType === "DSC") {
+        return "fas fa-sort-up";
+      }
+    }
+    else {
+      return "fas fa-sort";
+    }
+  }
+
   useInterval(() => {
     // Your custom logic here
-    if(new Date().getSeconds()===8){
+    if (new Date().getSeconds() === 8) {
       // callDataAPI()
       Axios.get(
         `/ETfLiveArbitrage/AllTickers`
@@ -108,7 +147,7 @@ const Live_Arbitrage_All_Table = (props) => {
       currentExpandedRows.concat(rowId);
     setExpandedRows(newExpandedRows);
   };
-  
+
   function renderItem(data) {
     const clickCallback = () => handleRowClick(data.symbol);
     const itemRows = [
@@ -145,37 +184,37 @@ const Live_Arbitrage_All_Table = (props) => {
                 <tr key={"row-expanded-" + data.id}>
                   <td> #1 </td>
                   <td>{data["ETFMover%1"][0]}</td>
-                  <td>{data["ETFMover%1"][1]}</td>
+                  <td className={data["ETFMover%1"][1].toFixed(3) < 0 ? "red" : "green"}>{data["ETFMover%1"][1].toFixed(3)}</td>
                   <td>{data["Change%1"][0]}</td>
-                  <td>{data["Change%1"][1]}</td>
+                  <td className={data["ETFMover%1"][1].toFixed(3) < 0 ? "red" : "green"}>{data["Change%1"][1].toFixed(3)}</td>
                 </tr>
                 <tr key={"row-expanded-" + data.id}>
                   <td> #2 </td>
                   <td>{data["ETFMover%2"][0]}</td>
-                  <td>{data["ETFMover%2"][1]}</td>
+                  <td className={data["ETFMover%2"][1].toFixed(3) < 0 ? "red" : "green"}>{data["ETFMover%2"][1].toFixed(3)}</td>
                   <td>{data["Change%2"][0]}</td>
-                  <td>{data["Change%2"][1]}</td>
+                  <td className={data["Change%2"][1].toFixed(3) < 0 ? "red" : "green"}>{data["Change%2"][1].toFixed(3)}</td>
                 </tr>
                 <tr key={"row-expanded-" + data.id}>
                   <td> #3 </td>
                   <td>{data["ETFMover%3"][0]}</td>
-                  <td>{data["ETFMover%3"][1]}</td>
+                  <td className={data["ETFMover%3"][1].toFixed(3) < 0 ? "red" : "green"}>{data["ETFMover%3"][1].toFixed(3)}</td>
                   <td>{data["Change%3"][0]}</td>
-                  <td>{data["Change%3"][1]}</td>
+                  <td className={data["Change%3"][1].toFixed(3) < 0 ? "red" : "green"}>{data["Change%3"][1].toFixed(3)}</td>
                 </tr>
                 <tr key={"row-expanded-" + data.id}>
                   <td> #4 </td>
                   <td>{data["ETFMover%4"][0]}</td>
-                  <td>{data["ETFMover%4"][1]}</td>
+                  <td className={data["ETFMover%4"][1].toFixed(3) < 0 ? "red" : "green"}>{data["ETFMover%4"][1].toFixed(3)}</td>
                   <td>{data["Change%4"][0]}</td>
-                  <td>{data["Change%4"][1]}</td>
+                  <td className={data["Change%4"][1].toFixed(3) < 0 ? "red" : "green"}>{data["Change%4"][1].toFixed(3)}</td>
                 </tr>
                 <tr key={"row-expanded-" + data.id}>
                   <td> #5 </td>
                   <td>{data["ETFMover%5"][0]}</td>
-                  <td>{data["ETFMover%5"][1]}</td>
+                  <td className={data["ETFMover%5"][1].toFixed(3) < 0 ? "red" : "green"}>{data["ETFMover%5"][1].toFixed(3)}</td>
                   <td>{data["Change%5"][0]}</td>
-                  <td>{data["Change%5"][1]}</td>
+                  <td className={data["Change%5"][1].toFixed(3) < 0 ? "red" : "green"}>{data["Change%5"][1].toFixed(3)}</td>
                 </tr>
               </tbody>
             </Table>
@@ -191,67 +230,101 @@ const Live_Arbitrage_All_Table = (props) => {
     <Card style={{ width: '100vh', height: '94vh' }}>
       <Card.Header className="text-white bg-color-dark flex-row">Live Arbitrage All ETFs
         <input
-              className="margin-left-auto d-inline-block"
-              name="search"
-              onChange={debounce(handleSearchChange, 500, { leading: true })}
-              value={searchString}
-            />
+          className="margin-left-auto d-inline-block"
+          name="search"
+          onChange={debounce(handleSearchChange, 500, { leading: true })}
+          value={searchString}
+          placeholder="Enter ETF to Search"
+        />
       </Card.Header>
       <Card.Body className="padding-0 bg-color-dark overflow-auto height-50vh font-size-sm">
         <Table className="overflow-auto" striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th className="cursor-pointer" onClick={()=>changeOrder("symbol")}>symbol
-                <span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span>
+              <th className="cursor-pointer">symbol
+                <a href="#">
+                  <i class={getClassNameForSortIcon("symbol")} onClick={() => changeOrder("symbol")}></i>
+                </a>
               </th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("Arbitrage in $")}>Arbitrage in $<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("Absolute Arbitrage")}>Absolute Arbitrage<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("ETF Price")}>ETF Price<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("ETF Change Price %")}>ETF Change Price %<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("ETF Trading Spread in $")}>ETF Trading Spread in $<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
-              <th className="cursor-pointer" onClick={()=>changeOrder("Net Asset Value Change%")}>Net Asset Value Change%<span>
-                  {isSorted
-                      ? orderType==='DSC'
-                        ? ' 🔼'
-                        : ' 🔽'
-                      : ''}
-                </span></th>
+              <th className="cursor-pointer">Arbitrage in $
+                <a href="#">
+                  <i class={getClassNameForSortIcon("Arbitrage in $")} onClick={() => changeOrder("Arbitrage in $")}></i>
+                </a>
+                <form class="form-inline">
+                  <div class="input-group mb-1 mr-sm-1">
+                    <select class="custom-select mb-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="=">=</option>
+                      <option value="!=">!=</option>
+                      <option value=">">&gt;</option>
+                      <option value=">=">&gt;=</option>
+                      <option value="<">&lt;</option>
+                      <option value="<=">&lt;=</option>
+                    </select>
+                    <input 
+                    type="text" 
+                    class="form-control mb-1 mr-sm-2" 
+                    id="inlineFormInputGroupUsername2" 
+                    placeholder="Arbitrage Value"
+                    // onChange={debounce(handleFilterChange, 500, { leading: true })}
+                    // value={"Arbitrage in $,"+inputArb.value+","+filterStringArb}
+                    />
+                  </div>
+                </form>
+              </th>
+              <th className="cursor-pointer">Absolute Arbitrage
+                <a href="#">
+                  <i class={getClassNameForSortIcon("Absolute Arbitrage")} onClick={() => changeOrder("Absolute Arbitrage")}></i>
+                </a>
+                <form class="form-inline">
+                  <div class="input-group mb-1 mr-sm-1">
+                    <select class="custom-select mb-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="=">=</option>
+                      <option value="!=">!=</option>
+                      <option value=">">&gt;</option>
+                      <option value=">=">&gt;=</option>
+                      <option value="<">&lt;</option>
+                      <option value="<=">&lt;=</option>
+                    </select>
+                    <input type="text" class="form-control mb-1 mr-sm-2" id="inlineFormCustomArbitrageAbs" placeholder="Arbitrage Value"/>
+                  </div>
+                </form>
+              </th>
+              <th className="cursor-pointer">ETF Price
+                <a href="#">
+                  <i class={getClassNameForSortIcon("ETF Price")} onClick={() => changeOrder("ETF Price")}></i>
+                </a>
+              </th>
+              <th className="cursor-pointer">ETF Change Price %
+                <a href="#">
+                  <i class={getClassNameForSortIcon("ETF Change Price %")} onClick={() => changeOrder("ETF Change Price %")}></i>
+                </a>
+              </th>
+              <th className="cursor-pointer">ETF Trading Spread in $
+                <a href="#">
+                  <i class={getClassNameForSortIcon("ETF Trading Spread in $")} onClick={() => changeOrder("ETF Trading Spread in $")}></i>
+                </a>
+                <form class="form-inline">
+                  <div class="input-group mb-1 mr-sm-1">
+                    <select class="custom-select mb-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                      <option selected>Choose...</option>
+                      <option value="=">=</option>
+                      <option value="!=">!=</option>
+                      <option value=">">&gt;</option>
+                      <option value=">=">&gt;=</option>
+                      <option value="<">&lt;</option>
+                      <option value="<=">&lt;=</option>
+                    </select>
+                    <input type="text" class="form-control mb-1 mr-sm-2" id="inlineFormInputGroupUsername2" placeholder="Arbitrage Value"/>
+                  </div>
+                </form>
+              </th>
+              <th className="cursor-pointer">Net Asset Value Change%
+                <a href="#">
+                  <i class={getClassNameForSortIcon("Net Asset Value Change%")} onClick={() => changeOrder("Net Asset Value Change%")}></i>
+                </a>
+              </th>
             </tr>
           </thead>
           <tbody>
