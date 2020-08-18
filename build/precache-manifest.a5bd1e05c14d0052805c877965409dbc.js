8f6bf8db4973cd73edf13a420eb5892852a1ef5a
Piyush <piyush888@gmail.com>
2020-08-18 22:28:49 +0530

    2nd article added

---
+++ build/precache-manifest.a5bd1e05c14d0052805c877965409dbc.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ed7a3eda2121fe72e825511bf52677",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac53e26f6f5a936f0143",
+    "url": "/static/css/11.046b4c08.chunk.css"
+  },
+  {
+    "revision": "7b2c096829a0d28b05f5",
+    "url": "/static/css/5.e1223e6f.chunk.css"
+  },
+  {
+    "revision": "7ca583ea592ba37610b4",
+    "url": "/static/css/7.652577f4.chunk.css"
+  },
+  {
+    "revision": "61f77d9f30530e5d0fe6",
+    "url": "/static/css/8.081c2f0c.chunk.css"
+  },
+  {
+    "revision": "02e558c2dc61902973f3",
+    "url": "/static/css/main.5c93f2d3.chunk.css"
+  },
+  {
+    "revision": "f36f91992ca103951012",
+    "url": "/static/js/0.012d2293.chunk.js"
+  },
+  {
+    "revision": "f01b6ee26155721017cc5fc8eb2b8875",
+    "url": "/static/js/0.012d2293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4049af615ac71fe505",
+    "url": "/static/js/1.4596779c.chunk.js"
+  },
+  {
+    "revision": "b5aeea546c13ae1af991",
+    "url": "/static/js/10.18404042.chunk.js"
+  },
+  {
+    "revision": "ac53e26f6f5a936f0143",
+    "url": "/static/js/11.916ff0e3.chunk.js"
+  },
+  {
+    "revision": "9cbb5d9390ebf269ea96",
+    "url": "/static/js/2.86ffe12d.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/2.86ffe12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2c096829a0d28b05f5",
+    "url": "/static/js/5.b635ab76.chunk.js"
+  },
+  {
+    "revision": "a4ad355058fa34d4c6acf09b37800d53",
+    "url": "/static/js/5.b635ab76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d577b3c6a8dc97d28f",
+    "url": "/static/js/6.80eac8dc.chunk.js"
+  },
+  {
+    "revision": "7ca583ea592ba37610b4",
+    "url": "/static/js/7.ff2c72e2.chunk.js"
+  },
+  {
+    "revision": "61f77d9f30530e5d0fe6",
+    "url": "/static/js/8.a2d4f92d.chunk.js"
+  },
+  {
+    "revision": "fda0d5c881685e540a51",
+    "url": "/static/js/9.76395879.chunk.js"
+  },
+  {
+    "revision": "02e558c2dc61902973f3",
+    "url": "/static/js/main.507fe6c0.chunk.js"
+  },
+  {
+    "revision": "60d50c406b666c55cd44",
+    "url": "/static/js/runtime-main.ad73a065.js"
+  },
+  {
+    "revision": "b4597ef3041410d11360b8d7e66616b8",
+    "url": "/static/media/AWS.b4597ef3.png"
+  },
+  {
+    "revision": "144daf09daa796c51f98244e2df02c67",
+    "url": "/static/media/AllETFLiveArbitrage.144daf09.png"
+  },
+  {
+    "revision": "f54f397f7ccdb1118c1cfa866f6c988e",
+    "url": "/static/media/BofaSymbol.f54f397f.jpg"
+  },
+  {
+    "revision": "faf1754e19ff70240a6072fd8c003cb8",
+    "url": "/static/media/CNBC.faf1754e.png"
+  },
+  {
+    "revision": "de8aa2700c19a4d72004e5ecf9751185",
+    "url": "/static/media/ETFDescription.de8aa270.png"
+  },
+  {
+    "revision": "8b02f3894b4e4477d81cd420809c980a",
+    "url": "/static/media/Fidelity.8b02f389.png"
+  },
+  {
+    "revision": "7c8def321e5c9661edee2b895703bb68",
+    "url": "/static/media/HistoricalArbitrage.7c8def32.png"
+  },
+  {
+    "revision": "f88c936e21b63d00ffcc929b090aef21",
+    "url": "/static/media/LiveArbitrage.f88c936e.png"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/MongoDb.09ed2051.png"
+  },
+  {
+    "revision": "9348e128c514152683d5c22f07d3e7bb",
+    "url": "/static/media/NYTimes.9348e128.png"
+  },
+  {
+    "revision": "d36735560b993f2e265ebf56df9701c4",
+    "url": "/static/media/Python.d3673556.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/React.580ffd26.png"
+  }
+]);
